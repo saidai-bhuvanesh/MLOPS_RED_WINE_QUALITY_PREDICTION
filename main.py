a17ca235b923cd419d2a6a0a4aaf4706cacec2e1
kallappa2005 <kallappakabbur874@gmail.com>
2026-01-24 22:44:41 +0530

    model trained successfully

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion Stage "
 
@@ -37,3 +38,13 @@ except Exception as e:
     logger.exception(e)
     raise e
     
+STAGE_NAME = "Model Trainer Stage "
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
